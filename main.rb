b179c21cad23b86ddb02bfba02f6e813b8f5fa7a
Michael Erdmann <michael.erdmann@snafu.de>
2018-12-09 07:43:28 +0100

    min code block added

---
+++ main.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+begin  
+        puts("Starting "+$0)
+        # put your code here
+        
+rescue  
+      puts 'Some unchatched exception did occure'  
+end  
+puts "Done"
+
+exit 100
